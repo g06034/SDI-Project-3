57ff3275a9a717c493dffcf67a9cf1f3ab8c485e
g06034 <christopherbattle@Christophers-MacBook-Pro.local>
2014-09-25 22:21:56 -0400

    Added Project 4 js file
    Addition of js file for Project 4.

---
+++ battle_christopher_assignment4.js
@@ -0,0 +1,7 @@
+// alert("JavaScript works!");
+
+
+/*---------------------------------------------------------------*/
+// Function that matches phone number pattern like this xxx-xxx-xxxx
+// There are a lot of different ways to do it, for instance we could use recursion and the indexOf function
+// but I believe this is the cleanest and simplest one
